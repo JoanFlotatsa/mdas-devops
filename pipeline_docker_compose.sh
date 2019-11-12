d74d1fcaf286866b3852c3e9275ec642379d8cb8
JoanFlotatsa <joanflotatsa@iqs.edu>
2019-11-12 22:03:25 +0100

    Kubernetes

--- pipeline_docker_compose.sh
+++ pipeline_docker_compose.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
-docker-compose rm -f && docker-compose up --build && docker-compose run --rm mytest && docker-compose push
+docker-compose rm -f && docker-compose up --build -d && docker-compose run --rm mytest && docker-compose push
+
+#deploy
+kubectl apply -f votingapp.yaml
+
+#test
+kubectl run mytests \
+--generator=run-pod/v1 \
+--image=joanflotatsa/votingapp-test \
+--env VOTINGAPP_HOST=myvotingapp.votingapp \
+--rm --attach --restart=Never
 
 echo "GREEN" || echo "RED"
